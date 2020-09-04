dc2ee268caac7f882ffef46ed280e3f5dacd6c66
NatanaelA <natanaela@xrvision.com>
2020-09-04 22:47:05 +0800

    print pdf from blazor

---
+++ Api/PDFController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PDFController : ControllerBase
+    {
+        private IConverter _converter;
+        public PDFController(IConverter converter)
+        {
+            _converter = converter;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string content = @"<html>
+                            <head></head>
+                            <body><h1>Test</h1></body>
+                            </html>";
+
+            var globalSettings = new GlobalSettings
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10 },
+                DocumentTitle = "PDF Report"
+            };
+            var objectSettings = new ObjectSettings
+            {
+                PagesCount = true,
+                HtmlContent = content,
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "css",  "styles.css") },
+                HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+                FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+            };
+            var pdf = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = { objectSettings }
+            };
+            var file = _converter.Convert(pdf);
+            return File(file, "application/pdf", "Test.pdf");
+        }
+    }
+}
