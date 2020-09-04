96c04926fa97887c89eeff4e27bef93a3dad75be
NatanaelA <natanaela@xrvision.com>
2020-09-04 22:58:14 +0800

    download pdf from button

---
+++ wwwroot/js/helper.ts
@@ -0,0 +1,7 @@
+﻿function downloadFromUrl(options: { url: string, fileName?: string }): void {
+    const anchorElement = document.createElement('a');
+    anchorElement.href = options.url;
+    anchorElement.download = options.fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+}
